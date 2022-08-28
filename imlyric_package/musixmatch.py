bb488d7acea15be277083e47e4e734ac8b091b99
Francesca Broccardo <884949@stud.unive.it>
2022-08-29 00:36:58 +0200

    [musixmatch] added new apis to print lyrics
    added apis to print lyrics given artist and track name

--- imlyric_package/musixmatch.py
+++ imlyric_package/musixmatch.py
@@ -8,6 +8,16 @@ MUSISXMATCH_URI = 'https://api.musixmatch.com/ws/1.1/'
 MUSISXMATCH_API_KEY = 'f5850d5632895b51bffdc58767606547'
 
 
+def get_lyrics_artist_track(artist, track):
+    url = MUSISXMATCH_URI + 'matcher.lyrics.get'
+    resp = requests.get(url, params={'q_track': track, 'q_artist': artist, 'apikey': MUSISXMATCH_API_KEY})
+    if resp.status_code == 200:
+        obj = resp.json()
+        if obj['message']['header']['status_code'] == 200:
+            return obj['message']['body']['lyrics']['lyrics_body']
+    raise Exception
+
+
 def get_lyrics_id(track_id):
     url = MUSISXMATCH_URI + 'track.lyrics.get'
     resp = requests.get(url, params={'track_id': track_id, 'apikey': MUSISXMATCH_API_KEY})
--- imlyric_package/tests/test_musixmatch.py
+++ imlyric_package/tests/test_musixmatch.py
@@ -24,3 +24,22 @@ class TestMusixmatch(TestCase):
         """
         res = get_lyrics_id(227377601)
         self.assertRegex(res, "When I find myself in times of trouble.*")
+
+    def test_get_lyrics_artist_track_none(self):
+        """
+        Tests get_lyrics_artist_track with None as input.
+        """
+        self.assertRaises(Exception, get_lyrics_artist_track, None, "a")
+
+    def test_get_lyrics_artist_track_wrong(self):
+        """
+        Tests get_lyrics_artist_track with a wrong input
+        """
+        self.assertRaises(Exception, get_lyrics_artist_track, "0i2ojwoirnrwe", "iwreirewirujr")
+
+    def test_get_lyrics_artist_track_right(self):
+        """
+        Tests get_lyrics_artist_track with a valid input.
+        """
+        res = get_lyrics_artist_track("The Beatles","Let it be")
+        self.assertRegex(res, "When I find myself in times of trouble.*")
