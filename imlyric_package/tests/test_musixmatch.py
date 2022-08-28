21cb8a4bf8db3aacf767c7834d88dde0b2753aa5
Francesca Broccardo <884949@stud.unive.it>
2022-08-29 00:54:29 +0200

    [imlyric] added api to find the track_id
    added api to find the track_id given artist, album and track names

--- imlyric_package/tests/test_musixmatch.py
+++ imlyric_package/tests/test_musixmatch.py
@@ -43,3 +43,28 @@ class TestMusixmatch(TestCase):
         """
         res = get_lyrics_artist_track("The Beatles","Let it be")
         self.assertRegex(res, "When I find myself in times of trouble.*")
+
+    def test_get_track_none(self):
+        """
+        Tests get_track with None as input.
+        """
+        self.assertRaises(Exception, get_track, None, None, "3oko33koww")
+
+    def test_get_track_wrong(self):
+        """
+        Tests get_track with a wrong input
+        """
+        self.assertRaises(Exception, get_track,"0i2ojwoirnrwe", "iwreirewirujr", "3oko33koww")
+
+    def test_get_track_right(self):
+        """
+        Tests get_track with a valid input.
+        """
+        track = get_track("The Beatles", "The Beatles Boxset", "Let it be")
+        artist_name, album_name = track['artist_name'], track['album_name']
+        track_name, track_id = track['track_name'], track['track_id']
+        self.assertEqual(artist_name, "The Beatles")
+        self.assertEqual(album_name, "The Beatles Boxset")
+        self.assertEqual(track_name, "Let It Be")
+        self.assertEqual(track_id, 227377601)
+
