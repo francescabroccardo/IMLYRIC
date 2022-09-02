6f4dae81e4ffbee15fb4dd1b6780b804a40949c5
iambrune <109245018+iambrune@users.noreply.github.com>
2022-09-02 13:59:03 +0200

    Update current_lyrics.py

--- current_lyrics.py
+++ current_lyrics.py
@@ -1,47 +1,48 @@
 import requests
 from musixmatch import Musixmatch
 
-# spotify authentication
-spo_getcurrent_url = 'https://api.spotify.com/v1/me/player/currently-playing'  # got by Spotify for Developers webpage
-spo_token = 'BQDpxsDbh2Frq-JGm9f4ZO_7EXqbe2kI0KbGBKRfVuzuqbgIrHOb8OP00yguqyoXyUADDBjyqr7c8e_Y3knLY6HUfV_SXZ_AnzGIlameBnY6QXR1GHJmNJn4zWR2AFw-srIhpnIiKlSJSk5TVLElfBfmhztbwjiX44sDPcAaEO9yhGeIrFIga7r1Exl0zrf37mjAUxXz'
 
+def get_curr_track_info():
+    token = 'BQDfXRTqChB9KaxvfyYHcV8TocDKvvxtxpNJ58b7nNohg1S8Q9TEaeKq0tmNIDmuqGt-xNd2uIBWqJUDmYijCX6GPGKLulPvezaHWxXMkBgdXvgmPaZoXAz37GBr4mlbV1P0XIdJBhwxH2YTXIe9H-Ypg8sN8b-C9yWwMBaJGx55ohXGJ3lbhYy17vhvXzo5Dv3VG4iO'
+    query = 'https://api.spotify.com/v1/me/player/currently-playing'  # got by Spotify for Developers webpage
 
-# Get current spotify track info
-def get_current_track(access_token):
     response = requests.get(
-        spo_getcurrent_url,
+        query,
         headers={'Authorization': f'Bearer {token}'}
     )
-    json_resp = response.json()
+    json_resp = response.json()['item']
 
-    track_id = json_resp['item']['id']
-    track_name = json_resp['item']['name']
-    artists = [artist for artist in json_resp['item']['artists']]
+    track_id = json_resp['id']
+    track_name = json_resp['name']
+    artists = [artist for artist in json_resp['artists']]
 
-    link = json_resp['item']['external_urls']['spotify']
+    link = json_resp['external_urls']['spotify']
 
     artist_names = ', '.join([artist['name'] for artist in artists])
     first_artist = artist_names.split(',')[0]
-    print(artist_names)
 
     current_track_info = {
-        "id": track_id,
-        "track_name": track_name,
-        "artist": first_artist,
-        "link": link
+        'id': track_id,
+        'track_name': track_name,
+        'artist': first_artist,
+        'link': link
     }
 
-    return current_track_info
-
-
-def get_info_song():
     current_track_id = None
-    current_track_info = get_current_track(spo_token)
 
     if current_track_info['id'] != current_track_id:
+        global song_name
+        global song_artist
+        global song_id
+
         song_name = current_track_info['track_name']
         song_artist = current_track_info['artist']
-    return song_name, song_artist
+        song_id = current_track_info['id']
+
+    else:
+        return 0
+
+    return song_id
 
 
 def get_lyrics(song, artist):
@@ -57,13 +58,16 @@ def get_lyrics(song, artist):
 
 
 def main():
-    get_current_track(access_token):
-    get_info_song()
-    get_lyrics(song_name, song_artist)
+    # canzone = get_curr_track_info(spo_token)[0]
+    # artista = get_curr_track_info(spo_token)[1]
+
+    canz_id = str(get_curr_track_info())
+    testo = get_lyrics(song_name, song_artist)
+
+    print(song_name + ', ' + song_artist + ' Song ID = ' + canz_id + ' ' + testo)
 
     return 1
 
 
 if __name__ == '__main__':
     main()
-    
